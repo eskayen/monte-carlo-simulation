6cd6e3f76dd15cd56816513a1c1a839755de094e
Saad K. Niazi <saadkn@macbook-pro.mynetworksettings.com>
2024-04-16 17:22:36 -0400

    basic stuff

--- simulation.py
+++ simulation.py
@@ -1,1 +1,12 @@
-import  matplotlib as pyplot
+import  matplotlib as plt
+
+# initial macros
+INITIAL_INVESTMENT = 1000000
+NUM_SIMULATIONS = 1000
+
+
+def main():
+    print("Main")
+
+if __name__ == "__main__":
+    main()

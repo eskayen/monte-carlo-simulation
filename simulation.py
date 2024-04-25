a6084579e5cba2fd510b912fae57abb023ed8a81
agrupac <agrupac@gmu.edu>
2024-04-25 15:25:52 -0400

    Added a second WIP optimal finder

--- simulation.py
+++ simulation.py
@@ -14,7 +14,9 @@ start_money = 0
 num_sims = 0
 num_years = 0
 profit_threshold = 0
+profit_prob = 0
 loss_threshold = 0
+loss_prob = 0
 
 # stock parameters
 stock_params = {
@@ -104,20 +106,25 @@ def get_inputs():
     num_years = int(entry_num_years.get())
     profit_threshold = start_money * (100 + int(entry_profit.get())) / 100
     loss_threshold = start_money * (100 - int(entry_loss.get())) / 100
-
-# function for main simulation logic
-def run_sim():
     
-    # get user inputs
-    get_inputs()
-
-    # determine what percentage of starting money went to which stock
-    stock_allocs = {
+def get_stock_allocs():
+    return {
         'A': scale_A.get() / 100,
         'B': scale_B.get() / 100,
         'C': scale_C.get() / 100,
         'D': scale_D.get() / 100
     }
+
+def run_sim_default():
+    stock_allocs = get_stock_allocs()
+    global num_sims
+    run_sim(stock_allocs, num_sims)
+
+# function for main simulation logic
+def run_sim(stock_allocs, num_sims):
+    
+    # get user inputs
+    get_inputs()
     
     # perform monte carlo simulation
     portfolio_values = [] # array to contain total money returned from each simulation
@@ -158,8 +165,18 @@ def run_sim():
     avg_profit.config(text=f'Average Profit: {mean_percent_profit:.2f}%')
     end_money.config(text=f'Average Ending Money: ${mean_profit:.2f}')
     standard_dev.config(text=f'Standard Deviation: ${std_dev:.2f}')
-    profit.config(text=f'Probability of at Least {int(entry_profit.get())}% Profit: {prob_profit * 100:.2f}%')
-    loss.config(text=f'Probability of at Least {int(entry_loss.get())}% Loss: {prob_loss * 100:.2f}%')
+
+    profit_percent = int(entry_profit.get())
+    if(profit_percent != 0):
+        profit.config(text=f'Probability of at Least {profit_percent}% Profit: {prob_profit * 100:.2f}%')
+    else:
+        profit.config(text=f'Probability of Any Profit: {prob_profit * 100:.2f}%')
+    
+    loss_percent = int(entry_loss.get())
+    if(loss_percent != 0):
+        loss.config(text=f'Probability of at Least {loss_percent}% Loss: {prob_loss * 100:.2f}%')
+    else:
+        loss.config(text=f'Probability of Any Loss: {prob_loss * 100:.2f}%')
 
     # generate histogram
     if cbtn_graph_bool.get() == True:
@@ -201,14 +218,16 @@ def find_opt():
     print(stocks_mean)
     print(stocks_std)
 
+    n = 100000
+
     # arrays holding the simulation results
-    all_weights = np.zeros((num_sims, 4))
-    mean_arr = np.zeros(num_sims)
-    stdev_arr = np.zeros(num_sims)
-    sharpe_ratios = np.zeros(num_sims)
+    all_weights = np.zeros((n, 4))
+    mean_arr = np.zeros(n)
+    stdev_arr = np.zeros(n)
+    sharpe_ratios = np.zeros(n)
 
     ## repeat for the number of simulations given
-    for i in range(num_sims):
+    for i in range(n):
         # create random weights for each stock that add up to 1
         weights = np.array(np.random.random(4))
         weights = weights / np.sum(weights)
@@ -245,6 +264,33 @@ def find_opt():
         t.set_bbox(dict(facecolor='purple', alpha=0.5, linewidth=0))
         plt.show()
 
+def find_opt_2():
+    print(f'\nPortfolio with\n\tat least {profit_prob * 100}% chance of {profit_threshold * 100}% or more profit,\n\tat least {loss_prob * 100}% chance of {loss_threshold * 100}% or more loss,\n\tand highest Sharpe ratio after {num_years} years:')
+
+    # TESTING
+    highest_sharpe = 0
+    num_sims = 100
+    opt_i = -1
+    i = 0
+    portfolio = ()
+
+    for percent_A in range(0, 101, 5):
+        for percent_B in range(0, 101 - percent_A, 5):
+            for percent_C in range(0, 101 - percent_A - percent_B, 5):
+                for percent_D in range(0, 101 - percent_A - percent_B - percent_C, 5):
+                    if (percent_A + percent_B + percent_C + percent_D == 100):
+                        (mean, stdev, p_prob, l_prob) = run_sim({'A' : percent_A, 'B' : percent_B, 'C' : percent_C, 'D' : percent_D}, num_sims)
+                        sharpe = mean / stdev
+
+                        if (p_prob >= profit_prob and l_prob <= loss_prob and sharpe >= highest_sharpe):
+                            opt_i = i
+                            highest_sharpe = sharpe
+                            portfolio = (percent_A, percent_B, percent_C, percent_D, sharpe)
+
+                        i += 1
+
+    if(portfolio == ()): portfolio = 'Does Not Exist'
+    print(f'\t\t{portfolio}\n')
 
 # main window
 canvas = Tk()
@@ -253,100 +299,116 @@ canvas.title('Monte Carlo Simulation')
 # starting money entry
 label_start_money = Label(canvas, text='Starting Money ($):')
 label_start_money.grid(row=0, column=0, padx=10, pady=5, sticky='w')
-default_text_SM = StringVar()
-default_text_SM.set(INITIAL_INVESTMENT)
-entry_start_money = Entry(canvas, textvariable=default_text_SM)
+text_SM = StringVar()
+text_SM.set(INITIAL_INVESTMENT)
+entry_start_money = Entry(canvas, textvariable=text_SM)
 entry_start_money.grid(row=0, column=1, padx=10, pady=5, sticky='e')
 
 # number of simulations entry
 label_num_sims = Label(canvas, text='Number of Simulations:')
 label_num_sims.grid(row=1, column=0, padx=10, pady=5, sticky='w')
-default_text_NS = StringVar()
-default_text_NS.set(NUM_SIMULATIONS)
-entry_num_sims = Entry(canvas, textvariable=default_text_NS)
+text_NS = StringVar()
+text_NS.set(NUM_SIMULATIONS)
+entry_num_sims = Entry(canvas, textvariable=text_NS)
 entry_num_sims.grid(row=1, column=1, padx=10, pady=5, sticky='e')
 
 # number of years entry
 label_num_years = Label(canvas, text='Time Period (Years):')
 label_num_years.grid(row=2, column=0, padx=10, pady=5, sticky='w')
-default_text_NY = StringVar()
-default_text_NY.set('1')
-entry_num_years = Entry(canvas, textvariable=default_text_NY)
+text_NY = StringVar()
+text_NY.set('1')
+entry_num_years = Entry(canvas, textvariable=text_NY)
 entry_num_years.grid(row=2, column=1, padx=10, pady=5, sticky='e')
 
 # desired profit entry
-label_profit = Label(canvas, text='Desired Profit (%):')
+label_profit = Label(canvas, text='Desired Profit Amount (%):')
 label_profit.grid(row=3, column=0, padx=10, pady=5, sticky='w')
-default_text_DP = StringVar()
-default_text_DP.set('0')
-entry_profit = Entry(canvas, textvariable=default_text_DP)
+text_DP = StringVar()
+text_DP.set('0')
+entry_profit = Entry(canvas, textvariable=text_DP)
 entry_profit.grid(row=3, column=1, padx=10, pady=5, sticky='e')
 
 # desired loss entry
-label_loss = Label(canvas, text='Desired Loss (%):')
+label_loss = Label(canvas, text='Desired Loss Amount (%):')
 label_loss.grid(row=4, column=0, padx=10, pady=5, sticky='w')
-default_text_DL = StringVar()
-default_text_DL.set('0')
-entry_loss = Entry(canvas, textvariable=default_text_DL)
+text_DL = StringVar()
+text_DL.set('0')
+entry_loss = Entry(canvas, textvariable=text_DL)
 entry_loss.grid(row=4, column=1, padx=10, pady=5, sticky='e')
 
-# button for displaying the distribution graphs of all stocks
-btn_dispay_stocks = Button(canvas, text='Show Stock Distributions', command=display_stocks)
-btn_dispay_stocks.grid(columnspan=2, pady=(15, 0))
+# desired profit probability entry
+label_p_prob = Label(canvas, text='Desired Profit Probability (%):')
+label_p_prob.grid(row=5, column=0, padx=10, pady=5, sticky='w')
+text_p_prob = StringVar()
+text_p_prob.set('0')
+entry_p_prob = Entry(canvas, textvariable=text_p_prob)
+entry_p_prob.grid(row=5, column=1, padx=10, pady=5, sticky='e')
+
+# desired loss probability entry
+label_l_prob = Label(canvas, text='Desired Loss Probability (%):')
+label_l_prob.grid(row=6, column=0, padx=10, pady=5, sticky='w')
+text_l_prob = StringVar()
+text_l_prob.set('0')
+entry_l_prob = Entry(canvas, textvariable=text_l_prob)
+entry_l_prob.grid(row=6, column=1, padx=10, pady=5, sticky='e')
 
 # scales for allocating percentages to each stock
 label_A = Label(canvas, text='Allocation for Stock A: 0%')
-label_A.grid(column=0, columnspan=2, padx=10, pady=(15, 0))
+label_A.grid(row=0, column=4, columnspan=2, padx=10)
 scale_A = Scale(canvas, from_=0, to=100, orient='horizontal', length=300, command=update_label_A, resolution=1, tickinterval=25)
-scale_A.grid(column=0, columnspan=2, padx=10)
+scale_A.grid(row=1, column=4, rowspan=2, columnspan=2, padx=10)
 
 label_B = Label(canvas, text='Allocation for Stock B: 0%')
-label_B.grid(column=0, columnspan=2, padx=10)
+label_B.grid(row=3, column=4, columnspan=2, padx=10)
 scale_B = Scale(canvas, from_=0, to=100, orient='horizontal', length=300, command=update_label_B, resolution=1, tickinterval=25)
-scale_B.grid(column=0, columnspan=2, padx=10)
+scale_B.grid(row=4, column=4, rowspan=2, columnspan=2, padx=10)
 
 label_C = Label(canvas, text='Allocation for Stock C: 0%')
-label_C.grid(column=0, columnspan=2, padx=10)
+label_C.grid(row=6, column=4, columnspan=2, padx=10)
 scale_C = Scale(canvas, from_=0, to=100, orient='horizontal', length=300, command=update_label_C, resolution=1, tickinterval=25)
-scale_C.grid(column=0, columnspan=2, padx=10)
+scale_C.grid(row=7, column=4, rowspan=2, columnspan=2, padx=10)
 
 label_D = Label(canvas, text='Allocation for Stock D: 0%')
-label_D.grid(column=0, columnspan=2, padx=10)
+label_D.grid(row=9, column=4, columnspan=2, padx=10)
 scale_D = Scale(canvas, from_=0, to=100, orient='horizontal', length=300, command=update_label_D, resolution=1, tickinterval=25)
-scale_D.grid(column=0, columnspan=2, padx=10, pady=(0, 10))
+scale_D.grid(row=10, column=4, rowspan=2, columnspan=2, padx=10)
+
+# button for displaying the distribution graphs of all stocks
+btn_dispay_stocks = Button(canvas, text='Show Stock Distributions', command=display_stocks)
+btn_dispay_stocks.grid(row=18, column=4, columnspan=2)
 
 # Create fields for results
 end_money = Label(canvas, text='Average Ending Money:')
-end_money.grid(column=0, columnspan=2, padx=10, pady=5)
+end_money.grid(row=7, column=0, columnspan=2, padx=10, pady=5)
 
 avg_profit = Label(canvas, text='Average Profit:')
-avg_profit.grid(column=0, columnspan=2, padx=10, pady=5)
+avg_profit.grid(row=8, column=0, columnspan=2, padx=10, pady=5)
 
 standard_dev = Label(canvas, text='Standard Deviation:')
-standard_dev.grid(column=0, columnspan=2, padx=10, pady=5)
+standard_dev.grid(row=9, column=0, columnspan=2, padx=10, pady=5)
 
 profit = Label(canvas, text='Probability of Min Desired Profit:')
-profit.grid(column=0, columnspan=2, padx=10, pady=5)
+profit.grid(row=10, column=0, columnspan=2, padx=10, pady=5)
 
 loss = Label(canvas, text='Probability of Min Desired Loss:')
-loss.grid(column=0, columnspan=2, padx=10, pady=5)
+loss.grid(row=11, column=0, columnspan=2, padx=10, pady=5)
 
 # button to run sim
-btn_run = Button(canvas, text='Run Simulation', command=run_sim)
-btn_run.grid(row=19, column=0, pady=10)
+btn_run = Button(canvas, text='Run Simulation', command=run_sim_default)
+btn_run.grid(row=18, column=0, pady=10)
 
 # checkbutton for displaying simulation results graph
 cbtn_graph_bool = IntVar()
 cbtn_graph = Checkbutton(canvas, text='Show Simulation Graph', variable=cbtn_graph_bool)
-cbtn_graph.grid(row=21, column=0, pady=10)
+cbtn_graph.grid(row=20, column=0, pady=10)
 
 # button to run optimal portfolio calculation
 btn_opt = Button(canvas, text='Find Optimal Portfolio', command=find_opt)
-btn_opt.grid(row=19, column=1, pady=10)
+btn_opt.grid(row=18, column=1, pady=10)
 
 # check button for displaying optimal portfolio graph
 cbtn_opt_bool = IntVar()
 cbtn_opt = Checkbutton(canvas, text='Show Optimal Portfolio Graph', variable=cbtn_opt_bool)
-cbtn_opt.grid(row=21, column=1, pady=10)
+cbtn_opt.grid(row=20, column=1, pady=10)
 
 canvas.mainloop()

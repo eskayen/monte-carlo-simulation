38e38c18df41a64edf7b6f37e39ec5bb1a544297
agrupac <agrupac@gmu.edu>
2024-04-26 01:04:21 -0400

    fixes

--- opt_finder_2.py
+++ opt_finder_2.py
@@ -1,12 +1,11 @@
 import numpy as np
 import time
 
-# runtime(seconds) = years * sims / 100
-NUM_SIMS = 1000 # adjust if too slow
+NUM_SIMS = 200 # adjust if too slow
 INITIAL_MONEY = 1000000
-NUM_YEARS = 1 # result will depend on years
+NUM_YEARS = 2 # result will depend on years
 STEP_SIZE = 5 # step size of permutations
-ALLOWED_RISK = 0.2 # chance that you will lose any amount of money
+ALLOWED_RISK = 0.15 # chance that you will lose any amount of money
 
 #SIMULATOR
 def sim(percent_A, percent_B, percent_C, percent_D):
@@ -58,35 +57,41 @@ def sim(percent_A, percent_B, percent_C, percent_D):
 
     # analyze results
     mean_profit = np.mean(portfolio_values)
-    std_dev = np.std(portfolio_values)
 
-    return (mean_profit, std_dev, risk)
+    return (mean_profit, risk)
 # END sim
 
+def find_opt():
+    print(f'\nPortfolio with highest mean and lowest risk after {NUM_YEARS} years (max allowed risk = {ALLOWED_RISK * 100:.0f}%):')
 
-print(f'\nPortfolio with highest mean and lowest risk after {NUM_YEARS} years (max allowed risk = {ALLOWED_RISK * 100:.0f}%):')
+    portfolio = ()
+    highest_mean = INITIAL_MONEY
+    lowest_risk = 1
 
-#highest_sharpe = 0
-portfolio = ()
-highest_mean = INITIAL_MONEY
-lowest_risk = 1
+    #start_time = time.time()
 
-start_time = time.time()
+    # get all permutations of stock allocations
+    for percent_A in range(0, 101, STEP_SIZE):
+        for percent_B in range(0, 101 - percent_A, STEP_SIZE):
+            for percent_C in range(0, 101 - percent_A - percent_B, STEP_SIZE):
+                for percent_D in range(0, 101 - percent_A - percent_B - percent_C, STEP_SIZE):
+                    # filter for permutations which sum to 100
+                    if (percent_A + percent_B + percent_C + percent_D == 100):
+                        # perform simulations with the current permutation
+                        (mean, risk) = sim(percent_A, percent_B, percent_C, percent_D)
+                        # search for the permutation which has the highest mean and lowest chance of risk
+                        if (mean >= highest_mean and risk >= 0 and risk <= ALLOWED_RISK):
+                            highest_mean = mean
+                            lowest_risk = risk
+                            portfolio = (percent_A, percent_B, percent_C, percent_D)
+                            
 
-for percent_A in range(0, 101, STEP_SIZE):
-    for percent_B in range(0, 101 - percent_A, STEP_SIZE):
-        for percent_C in range(0, 101 - percent_A - percent_B, STEP_SIZE):
-            for percent_D in range(0, 101 - percent_A - percent_B - percent_C, STEP_SIZE):
-                if (percent_A + percent_B + percent_C + percent_D == 100):
-                    (mean, stdev, risk) = sim(percent_A, percent_B, percent_C, percent_D)
-                    # search for the permutation which has the highest mean and lowest chance of loss/highest chance of profit
-                    if (mean >= highest_mean and risk >= 0 and risk <= ALLOWED_RISK):
-                        highest_mean = mean
-                        lowest_risk = risk
-                        portfolio = (percent_A, percent_B, percent_C, percent_D)
-                        
+    #end_time = time.time()
+    #runtime = end_time - start_time
+    profit = (highest_mean - INITIAL_MONEY) / INITIAL_MONEY * 100
+    print(f'\t\t{portfolio}\n\t\tExpected Profit: ~{profit:.2f}%\n\t\tChance of Risk: ~{lowest_risk:.3f}%')
+    #print(f"Runtime: {runtime:.2f} seconds")
+# END find_opt
 
-end_time = time.time()
-runtime = end_time - start_time
-print(f'\t\t{portfolio}\n\t\tExpected Profit: ~{(highest_mean - INITIAL_MONEY) / INITIAL_MONEY * 100:.2f}%\n\t\tChance of Risk: ~{lowest_risk:.2f}%')
-print(f"Runtime: {runtime} seconds")
+
+find_opt()
